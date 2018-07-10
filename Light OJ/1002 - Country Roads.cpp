df06e28e435e2ef07044ed3189e9d8beacc94199
gmehedi <40127136+gmehedi@users.noreply.github.com>
2018-07-10 21:36:43 +0600

    Update 1002 - Country Roads.cpp

--- Light OJ/1002 - Country Roads.cpp
+++ Light OJ/1002 - Country Roads.cpp
@@ -1,72 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int>graph[510];
-int cost[510][510];
-int group[510];
-int cas=1;
-int res[510];
-struct str
-{
-    int x;
-    int y;
-    int z;
-};
-vector<str>v;
-bool compare(str a, str b)
-{
-    return a.z<b.z;
-}
+
+typedef pair<int,int>p;
+vector< p >graph[510];
 bool sign[510];
-int flag=0;
+int res[510],group[510],cas=1;
 
 int bfs(int u)
 {
-    queue<int>q;
-    q.push(u);
     sign[u]=false;
     res[u]=0;
-    while(!q.empty())
+    queue<pair<int,int> >q;
+    q.push( make_pair(u,0) );
+
+    while( ! q.empty() )
     {
-        int f=q.front();
+        int f=q.front().first;
+        int cost=q.front().second;
         q.pop();
         for(int i=0; i<(int)graph[f].size(); i++)
         {
-            int v=graph[f][i];
-            int c=cost[f][v];
-           // cout<<"F  "<<f<<"   "<<v<<  "   "<<cost[f][v]<<endl;
+            int v=graph[f][i].first;
+            int ww=graph[f][i].second;
+
             if(sign[v])
             {
+                 if(res[f]>ww)
+                {
+                    res[v]=res[f];
+                    q.push(make_pair(v,res[v]));
+                }
+                else
+                {
+                    res[v]=ww;
+                    q.push(make_pair(v,res[v]));
+                }
                 sign[v]=false;
-                q.push(v);
-              //  cout<<"W                                   "<<res[v]<<"   "<<cost[u][v]<<endl;
-                res[v]=max(res[f],cost[f][v]);
             }
+
         }
     }
 }
 
-int find_root(int n)
+void  print(int n)
 {
-    while( n != group[n]) group[n]=group[group[n]], n=group[n];
+    printf("Case %d:\n",cas++);
+    for(int i=0; i<n; i++)
+    {
+        if( res[i]==INT_MAX ) printf("Impossible\n");
+        else printf("%d\n",res[i]);
+    }
+}
+void initialize()
+{
+    memset(sign,true,sizeof sign);
+    for(int i=0; i<501; i++) group[i]=i,res[i]=INT_MAX;
 }
 
-void initialize(int n)
+int root(int i)
 {
-    for(int i=0; i<n; i++)group[i]=i;
+    while(i != group[i] )group[i]=group[group[i]], i=group[i];
+    return i;
 }
 
-void minimum_spannig_tree(int ra, int rb)
+bool unioon(int a, int b)
 {
+    int ra=root(a);
+    int rb=root(b);
+   // cout<<"RRR               "<<a<<"  "<<b<<"  "<<ra<<"  "<<rb<<endl;
+    if(ra==rb) return false;
     group[ra]=group[rb];
+  //  cout<<group[ra]<<"  "<<group[rb]<<endl;
+    return true;
 }
 
-void graph_build(int x, int y, int w)
+void build(int x,int y, int w)
 {
-  //  cout<<"GGG     "<<x<<"    "<<y<<endl;
-    graph[x].push_back(y);
-    graph[y].push_back(x);
-    cost[x][y]=w;
-    cost[y][x]=w;
+    graph[x].push_back( make_pair(y,w) );
+    graph[y].push_back( make_pair(x,w) );
 }
 
 int main()
@@ -76,43 +87,31 @@ int main()
     while(t--)
     {
         int n,m;
+        vector< pair<int,p > >v;
         scanf("%d%d",&n,&m);
+        initialize();
         for(int i=0; i<m; i++)
         {
-            int t1,t2,w;
-            scanf("%d%d%d",&t1,&t2,&w);
-            v.push_back({t1,t2,w});
-          //  cout<<"H "<<i<<"    "<<m<<endl;
+            int x,y,w;
+            scanf("%d%d%d",&x,&y,&w);
+            v.push_back( make_pair( w,make_pair(x,y) ) );
         }
-        int destination;
-        scanf("%d",&destination);
-        sort(v.begin(),v.end(),compare);
-        initialize(n);
+        sort(v.begin(),v.end());
         for(int i=0; i<(int)v.size(); i++)
         {
-            int t1=0,t2=0,w=0;
-            t1=v[i].x, t2=v[i].y, w=v[i].z;
-            int ra=find_root(t1);
-            int rb=find_root(t2);
-            if(ra==rb) continue;
-            else { minimum_spannig_tree(ra,rb); graph_build(t1,t2,w); }
+            int t1,t2,t3;
+            t1=v[i].second.first, t2=v[i].second.second, t3=v[i].first;
+          //  cout<<"T  "<<t1<<" "<<t2<<endl;
+            bool check=unioon(t1,t2);
+            if( check ) build(t1,t2,t3);
         }
-        printf("Case %d:\n",cas++);
-
-        for(int i=0; i<=n; i++)res[i]=INT_MAX;
-        memset(sign,true,sizeof sign);
-
-        bfs(destination);
-
-       for(int i=0; i<n; i++)
-       {
-           if(res[i]==INT_MAX) printf("Impossible\n");
-           else printf("%d\n",res[i]);
-       }
-
-       v.clear();
-        for(int i=0; i<n; i++) graph[i].clear();
-        memset(cost,-1,sizeof cost);
+        int t;
+        scanf("%d",&t);
+        //for(int i=0; i<n; i++)cout<<"R  "<<res[i]<<endl;
+        bfs(t);
+        print(n);
+       // for(int i=0; i<m; i++)   cout<<v[i].first<<"  "<<v[i].second.first<<"  "<<v[i].second.second<<endl;
+       for(int i=0; i<n; i++)graph[i].clear();
     }
     return 0;
 }
