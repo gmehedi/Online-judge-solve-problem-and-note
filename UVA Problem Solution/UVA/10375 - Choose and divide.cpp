1889142d54161d6ecd6cc5a85cae133685ae7fa1
M M Mehedi Hasan <40127136+gmehedi@users.noreply.github.com>
2018-10-10 05:10:01 +0600

    Update 10375 - Choose and divide.cpp

--- UVA Problem Solution/UVA/10375 - Choose and divide.cpp
+++ UVA Problem Solution/UVA/10375 - Choose and divide.cpp
@@ -6,22 +6,18 @@ int main()
     int p,q,r,s;
     while( scanf("%d %d %d %d",&p,&q,&r,&s) != EOF )
     {
-
         if(q>(p-q)) q=p-q;
         if(s>(r-s)) s=r-s;
         double result=1.0;
-       // cout<<"F "<<s<<" "<<q<<endl;
-       for (int i = 1; i <= q || i <= s; ++i)
+        // cout<<"F "<<s<<" "<<q<<endl;
+        for (int i = 1; i <= q || i <= s; ++i)
         {
             if (i <= q) result = result * (p - q + i) / i;
             if (i <= s) result = (result / (r - s + i)) * i;
         }
+        
         printf("%.5f\n",result);
-
     }
-
-
-
+    
     return 0;
-
 }
