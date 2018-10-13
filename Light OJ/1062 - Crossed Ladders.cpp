275111cf9fc8417981373542b9e228ad9368618f
M M Mehedi Hasan <40127136+gmehedi@users.noreply.github.com>
2018-10-13 06:59:51 +0600

    12-10-2018

---
+++ Light OJ/1062 - Crossed Ladders.cpp
@@ -0,0 +1,90 @@
+
+//M M Mehedi Hasan
+//From BUBT
+#include <bits/stdc++.h>
+
+#define pii              pair <int,int>
+#define pll              pair <long long,long long>
+#define sc               scanf
+#define pf               printf
+#define Pi               2*acos(0.0)
+#define ms(a,b)          memset(a, b, sizeof(a))
+#define pb(a)            push_back(a)
+#define MP               make_pair
+#define db               double
+#define ll               long long
+#define ull               unsigned long long
+#define ff               first
+#define ss               second
+#define sqr(x)           (x)*(x)
+#define VI               vector <int>
+#define DBG              pf("Hi\n")
+#define MOD              1000000007
+#define SZ(a)            (int)a.size()
+#define sf(a)            scanf("%d",&a)
+#define sfl(a)           scanf("%lld",&a)
+#define sff(a,b)         scanf("%d %d",&a,&b)
+#define sffl(a,b)        scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c)
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define loop1(i,n)       for(int i=1;i<=n;i++)
+#define REP(i,a,b)       for(int i=a;i<b;i++)
+#define RREP(i,a,b)      for(int i=a;i>=b;i--)
+#define intlim           2147483648
+#define ull              unsigned long long
+#define gcd(a, b)        __gcd(a, b)
+#define lcm(a, b)        ((a)*((b)/gcd(a,b)))
+#define maxl 100010
+
+using namespace std;
+
+
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+/*-----------------------Bitmask------------------*/
+//int Set(int N,int pos){return N=N | (1<<pos);}
+//int reset(int N,int pos){return N= N & ~(1<<pos);}
+//bool check(int N,int pos){return (bool)(N & (1<<pos));}
+/*------------------------------------------------*/
+
+double calculate(double x, double y, double w)
+{
+ return ( sqrt(x*x-w*w)*sqrt(y*y-w*w) )/(sqrt(x*x-w*w)+sqrt(y*y-w*w));
+}
+
+double Binary(double x, double y, double c)
+{
+    double left=0.0,mid =0,val=0.0,right= min(x,y);
+
+    while( fabs(left-right)>0.0000001 )
+    {
+        mid=(left+right)/2.0;
+
+        val=calculate(x,y,mid);
+        if(val<c) right=mid;
+        else left=mid;
+    }
+
+    return mid;
+}
+int main()
+{
+    int t,cas=1;
+    scanf("%d",&t);
+    while(t--)
+    {
+        double x,y,c;
+        scanf("%lf %lf %lf",&x,&y,&c);
+        printf("Case %d: %.10f\n",cas++,Binary(x,y,c));
+    }
+
+    return 0;
+}
