2970b86e96c326424a9751bebfc01ef7e2f3818d
M M Mehedi Hasan <40127136+gmehedi@users.noreply.github.com>
2018-11-13 02:07:19 +0600

    13-11-2018

---
+++ Light OJ/1341 - Aladdin and the Flying Carpet.cpp
@@ -0,0 +1,113 @@
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+#define pii              pair <int,int>
+#define pll              pair <long long,long long>
+#define sc               scanf
+#define pf               printf
+#define Pi               2*acos(0.0)
+#define ms(a,b)         memset(a, b, sizeof(a))
+#define pb(a)            push_back(a)
+#define MP               make_pair
+#define db               double
+#define ll               long long
+#define ull               unsigned long long
+#define f               first
+#define s              second
+#define sqr(x)           (x)*(x)
+#define VI               vector <int>
+#define DBG              pf("Hi\n")
+#define MOD              1000000007
+#define SZ(a)            (int)a.size()
+#define sf(a)            scanf("%d",&a)
+#define sfl(a)           scanf("%lld",&a)
+#define sff(a,b)         scanf("%d %d",&a,&b)
+#define sffl(a,b)        scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c)
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define loop1(i,n)       for(int i=1;i<=n;i++)
+#define REP(i,a,b)       for(int i=a;i<b;i++)
+#define RREP(i,a,b)      for(int i=a;i>=b;i--)
+#define intlim           2147483648
+#define ull              unsigned long long
+#define gcd(a, b)        __gcd(a, b)
+#define lcm(a, b)        ((a)*((b)/gcd(a,b)))
+#define mk(x1,y1) make_pair(x1, y1)
+#define maxl 1000010
+using namespace std;
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+/*-----------------------Bitmask------------------*/
+//int Set(int N,int pos){return N=N | (1<<pos);}
+//int reset(int N,int pos){return N= N & ~(1<<pos);}
+//bool check(int N,int pos){return (bool)(N & (1<<pos));}
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdin);
+/*------------------------------------------------*/
+using namespace std;
+vector<int>prime;
+bool sign[maxl+2];
+
+void Prime()
+{
+    memset(sign,true,sizeof sign);
+
+    for(int i=3; i*i<=maxl; i+=2)
+    {
+       if(sign[i]) for(int j=i*i; j<=maxl; j+=(i*2)) sign[j]=false;
+    }
+    prime.push_back(2);
+    for(int i=3; i<=maxl; i+=2) if(sign[i]) prime.push_back(i);
+}
+
+ll Divisor(ll n)
+{
+    ll temp=n,tot=1;
+    int len=prime.size();
+    for(int i=0; prime[i]*prime[i] <= n && i<len; i++)
+    {
+        int cnt=0;
+        if(temp% prime[i] == 0)
+        {
+            while(temp%prime[i] == 0) temp /= prime[i], cnt++;
+            tot = ( tot*(cnt+1) );
+        }
+        if(temp<prime[i]) break;
+    }
+    if(temp > 1) tot *=2;
+    return tot;
+}
+
+
+int main()
+{
+    int t,cas=1;
+    Prime();
+
+    scanf("%d",&t);
+    while(t--)
+    {
+        ll a,b;
+        scanf("%lld %lld",&a,&b);
+        ll ans=Divisor(a);
+        ll sq=sqrt(b);
+        if((b*b) >= a) ans=0;
+        else
+        {
+            ans=ans/2;
+            for(int i=1; i<b; i++) if(a%i == 0) ans--;
+        }
+
+        printf("Case %d: %lld\n",cas++,ans);
+
+    }
+    return 0;
+}
