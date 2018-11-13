8424caa83b70138008d85750b26ec5523f6c28ce
M M Mehedi Hasan <40127136+gmehedi@users.noreply.github.com>
2018-11-14 03:12:29 +0600

    13-11-2018

---
+++ Light OJ/1163 - Bank Robbery.cpp
@@ -0,0 +1,77 @@
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+#define pii              pair <int,int>
+#define pll              pair <long long,long long>
+#define sc               scanf
+#define pf               printf
+#define Pi               2*acos(0.0)
+#define ms(a,b)         memset(a, b, sizeof(a))
+#define pb(a)            push_back(a)
+#define MP               make_pair
+#define db               double
+#define ll               long long
+#define ull               unsigned long long
+#define f               first
+#define s              second
+#define sqr(x)           (x)*(x)
+#define VI               vector <int>
+#define DBG              pf("Hi\n")
+#define MOD              1000000007
+#define SZ(a)            (int)a.size()
+#define sf(a)            scanf("%d",&a)
+#define sfl(a)           scanf("%lld",&a)
+#define sff(a,b)         scanf("%d %d",&a,&b)
+#define sffl(a,b)        scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c)      scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c)
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define loop1(i,n)       for(int i=1;i<=n;i++)
+#define REP(i,a,b)       for(int i=a;i<b;i++)
+#define RREP(i,a,b)      for(int i=a;i>=b;i--)
+#define intlim           2147483648
+#define ull              unsigned long long
+#define gcd(a, b)        __gcd(a, b)
+#define lcm(a, b)        ((a)*((b)/gcd(a,b)))
+#define mk(x1,y1) make_pair(x1, y1)
+#define maxl 1000010
+using namespace std;
+/*----------------------Graph Moves----------------*/
+//const int fx[]={+1,-1,+0,+0};
+//const int fy[]={+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+/*-----------------------Bitmask------------------*/
+//int Set(int N,int pos){return N=N | (1<<pos);}
+//int reset(int N,int pos){return N= N & ~(1<<pos);}
+//bool check(int N,int pos){return (bool)(N & (1<<pos));}
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdin);
+/*------------------------------------------------*/
+using namespace std;
+int main()
+{
+    int t,cas=1;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        ull n;
+        scanf("%llu",&n);
+        printf("Case %d:",cas++);
+        for(int i=9; i>=0; i--)
+        {
+            ull temp=(10*n)-i;
+            if(temp%9 == 0)
+            {
+                printf(" %llu",temp/9);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
